d6574ca49fbe1aeacc3d477eb1c16619e5eb228d
Petr Shumilov <petr1shum@gmail.com>
2015-10-02 04:51:03 +0900

    Fix some bugs

--- task1/task1.c
+++ task1/task1.c
@@ -56,7 +56,12 @@ int conditional(int x, int y, int z)
     x |= x << 2;
     x |= x << 4;
     x |= x << 8;
-    x |= x << 15;
+    x |= x << 16;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
     return (x & y) | (~x & z);
 }
 
