888c9c37600dec1727724090b792a5cabe12efc9
Petr Shumilov <petr1shum@gmail.com>
2015-10-09 15:21:06 +0900

    bitAnd

---
+++ task1/bitAnd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int bit_and(int a, int b)
+{
+    return ~(~a | ~b);
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", bit_and(a, b));
+    return 0;
+}
