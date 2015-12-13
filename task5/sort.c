befbb3863921a63271afe64502b8b542f515d715
Petr Shumilov <petr1shum@gmail.com>
2015-12-13 03:02:52 +0300

    Fix bugs

--- task5/sort.c
+++ task5/sort.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 const int N[9] = {5, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 const int MOD = 100000;
 
@@ -17,7 +16,6 @@ inline void print(int *arr, int _size)
 void sort0(int *arr, int _size) // ~O(n)
 {
     int *count = (int*)malloc((MOD + 0) * sizeof(int));
-
     int i, j = 0;
     for (i = 0; i < _size; ++i)
         count[arr[i]]++;
@@ -32,42 +30,66 @@ void sort0(int *arr, int _size) // ~O(n)
     free(count);
 }
 
-void sort1(int* arr, int l, int r, int _size)  //O(n log n)
-{
-    int i = l,
-    j = r,
-    m = arr[(l + r) / 2];
 
+void sort1(int *arr, int _size)
+{
+    long i, j, lb, ub, starrckpos = 1, ppos, pivot, temp, lbstarrck[2048], ubstarrck[2048]; 
+    lbstarrck[1] = 0;
+    ubstarrck[1] = _size - 1;
     do
     {
-        while (arr[i] < m)
-            i++;
-        while (arr[j] > m)
-            j--;
-        if (i <= j)
+        lb = lbstarrck[starrckpos];
+        ub = ubstarrck[starrckpos];
+        starrckpos--;
+        do
         {
-            if (arr[i] > arr[j])
+            ppos = (lb + ub) >> 1;
+            i = lb;
+            j = ub;
+            pivot = arr[ppos];
+            do
             {
-                int swp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = swp;
+                while (arr[i] < pivot) i++;
+                while (pivot < arr[j]) j--;
+                if (i <= j)
+                {
+                    temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+                    i++; j--;
+                }
+            } while (i <= j);
+            
+            if (i < ppos)
+            {     
+                if (i < ub )
+                {   
+                    starrckpos++;     
+                    lbstarrck[starrckpos] = i;
+                    ubstarrck[starrckpos] = ub;
+                }
+                ub = j;             
             }
-            i++;
-            j--;
-        }
-    } while (i <= j);
-
-    if (i < r)
-        sort1(arr, i, r, _size);
-    if (l < j)
-        sort1(arr, l, j, _size);
-
-    //if (l == 0 && r == _size)
-    //    print(arr, _size);
+            else
+            {
+                if (j > lb)
+                { 
+                    starrckpos++;
+                    lbstarrck[starrckpos] = lb;
+                    ubstarrck[starrckpos] = j;
+                }
+                lb = i;
+            }
+        } while (lb < ub);       
+    } while (starrckpos);  
 }
+
 
 void sort2(int *arr, int _size) //O(n ^ 2)
 {
+    if (_size > 1e5)
+    {
+        printf(" n/a ");
+        return;
+    }
     int i, j;
     for (i = 0; i < _size; ++i)
         for (j = 0; j < _size - 1; ++j)
@@ -77,35 +99,44 @@ void sort2(int *arr, int _size) //O(n ^ 2)
                 arr[j] = arr[j + 1];
                 arr[j + 1] = swp;
             }
-    //print(arr, _size);
 }
+
 
 int main()
 {
-    freopen("benchmark.txt", "a+", stdout);
-    
-    int k = 0;
-    for (k = 0; k < 9; ++k)
-    {
-        double init = clock();
-        
-        int *a = (int*)malloc(N[k] * sizeof(int));
-        srand(time(NULL));
-        int i;
-        for (i = 0; i < N[k]; ++i)
-            *(a + i) = rand() % MOD;
 
-        sort0(a, N[k]);
-        //sort1(a, 0, N[k], N[k]);
-        //sort2(a, N[k]);
-        
-        free(a);
-        
-        double _time = (clock() - init) / CLOCKS_PER_SEC;
-        printf("%.3f ", _time);
-        fflush(stdout); 
+    freopen("benchmark.txt", "w+", stdout);
+
+    void (*sorting_func[3])(int *arr, int _size) = {sort0, sort1, sort2};
+    char *header = "          |  5  | 10  | 100 | 1e3 | 1e4 | 1e5 | 1e6 | 1e7 | 1e8 |";
+    char *li[3] = {"O(n)       ", "O(n log n) ", "O(n ^ 2)   " };
+    
+    int f;
+    printf("BENCHMARK OF SORTINGS\n%s\n", header);
+    for (f = 0; f < 3; ++f)
+    {
+        printf("%s", li[f]);
+        int k;
+        for (k = 0; k < 9; ++k)
+        {
+            int *a = (int*)malloc(N[k] * sizeof(int));
+            srand(time(NULL));
+            int i;
+            for (i = 0; i < N[k]; ++i)
+                a[i] = rand() % MOD;
+
+            double init = clock();
+            sorting_func[f](a, N[k]);
+            double _time = (clock() - init) / CLOCKS_PER_SEC;
+            
+            free(a);
+            if (f == 2 && N[k] > 1e5)
+                continue;
+            printf("%.3f ", _time);
+            fflush(stdout); 
+        }
+        printf("\n");
     }
-    printf("\n");
     
     return 0;
 }
